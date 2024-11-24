77548e3c72a260e22d950c6fbdd5ff23f7ec1080
ilyhel <mzoughi.ilyes@gmail.com>
2024-11-24 12:10:36 -0500

    test1

--- main.tf
+++ main.tf
@@ -8,17 +8,7 @@ terraform {
 
 
 provider "powerplatform" {
-  # Use a service principal to authenticate with the Power Platform service
-  client_id     = var.client_id
-  client_secret = var.client_secret
-  tenant_id     = var.tenant_id
+
 }
 
-resource "powerplatform_environment" "development" {
-  display_name     = "example_environment"
-  location         = "europe"
-  azure_region     = "northeurope"
-  environment_type = "Developer"
-  environment_group_id = ""
- 
-}
+data "powerplatform_environments" "all_environments" {}
